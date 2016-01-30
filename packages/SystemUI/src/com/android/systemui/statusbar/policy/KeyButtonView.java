a7326abf93696f8d4d75e11e32e3a7a6c9f14b0d
LorDClockaN <davor@losinj.com>
2016-01-30 14:47:31 +0100

    Fix back button tinting:
    Add a method for retrieving custom color from KeyButtonView
    
    Change-Id: I7183c1da95782b2496110afdc1434aa3e0f99f63

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
@@ -72,6 +72,7 @@ public class KeyButtonView extends ImageView {
     private AudioManager mAudioManager;
     private boolean mGestureAborted;
     private boolean mShouldTintIcons = true;
+    private static int color;
 
     private boolean mPerformedLongClick;
 
@@ -335,13 +336,18 @@ public class KeyButtonView extends ImageView {
     public void setTint(boolean tint) {
         setColorFilter(null);
         if (tint) {
-            int color = Settings.System.getInt(mContext.getContentResolver(),
+            color = Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.NAVIGATION_BAR_TINT, -1);
             if (color != -1) {
+                reportColor();
                 setColorFilter(color);
             }
         }
         mShouldTintIcons = tint;
+    }
+
+    public static int reportColor() {
+        return color;
     }
 
     class SettingsObserver extends ContentObserver {
