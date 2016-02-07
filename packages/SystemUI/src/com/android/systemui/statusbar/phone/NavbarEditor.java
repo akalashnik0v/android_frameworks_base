eb39479c9daa63b683c2d55dce64e2ca2b9bfbf1
t-rex600 <fisch890@gmail.com>
2016-02-07 20:28:02 +0100

    fix Nav

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavbarEditor.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavbarEditor.java
@@ -145,7 +145,6 @@ public class NavbarEditor implements View.OnTouchListener {
             R.string.navbar_power_button, R.string.accessibility_power,
             KeyEvent.KEYCODE_POWER, R.drawable.ic_sysbar_power, R.drawable.ic_sysbar_power_land,
             R.drawable.ic_sysbar_power_side);
-
     private static final ButtonInfo[] ALL_BUTTONS = new ButtonInfo[] {
         NAVBAR_EMPTY, NAVBAR_HOME, NAVBAR_BACK, NAVBAR_SEARCH,
         NAVBAR_RECENT, NAVBAR_CONDITIONAL_MENU, NAVBAR_ALWAYS_MENU, NAVBAR_MENU_BIG, NAVBAR_POWER
